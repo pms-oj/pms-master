41a445cb8a4b0de4d4e554ed070d58dabeead069
Yeri Kim <laplacianmatrix@gmail.com>
2022-09-27 03:46:09 +0000

    제발

--- src/handler.rs
+++ src/handler.rs
@@ -55,12 +55,12 @@ impl State {
     ) -> async_std::io::Result<()> {
         match packet.heady.header.command {
             Command::HANDSHAKE => {
-                if let Ok(client_pubkey) = bincode::DefaultOptions::new().with_big_endian().deserialize::<PublicKey>(&packet.heady.body) {
+                if let Ok(client_pubkey) = bincode::DefaultOptions::new().with_big_endian().with_fixint_encoding().deserialize::<PublicKey>(&packet.heady.body) {
                     self.shared =
                         Arc::new(Mutex::new(Some(self.key.diffie_hellman(&client_pubkey))));
                     let req_packet = Packet::make_packet(
                         Command::HANDSHAKE,
-                        bincode::DefaultOptions::new().with_big_endian().serialize(&self.key.public_key()).unwrap(),
+                        bincode::DefaultOptions::new().with_big_endian().with_fixint_encoding().serialize(&self.key.public_key()).unwrap(),
                     );
                     req_packet.send(Pin::new(&mut stream)).await
                 } else {
